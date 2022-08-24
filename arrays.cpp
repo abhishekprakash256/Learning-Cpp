9eeb00a9528871cff19d3cb69440e533867795ed
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-08-24 13:21:09 -0400

    code

--- arrays.cpp
+++ arrays.cpp
@@ -1,10 +1,23 @@
 // making the array in the cpp 
 
-#include<iostreams>
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
+	int test_array[] = {1,2,3,4,5,6,7} ;
+
+	cout << test_array[1] << endl ; 
+
+	// make the array and usig it later
 	
+	int test_array_2[20];
+
+	test_array_2[0] = 1 ;
+
+	cout << test_array_2[0] << endl;
+
+
 }
+
