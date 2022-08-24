4b8fef42adadc776ff6746279fa2460efa21fc4c
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-08-24 10:11:10 -0400

    files

---
+++ data_types.cpp
@@ -0,0 +1,16 @@
+// the data types of the c++
+
+#include<iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	char grade = 'A';
+	string name = "Tom";
+
+
+	return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	string name = "Tom";
+	int age = 20;
+
+	cout << "hello " << endl;
+	cout << "The name is " << name << endl ;
+
+	return 0;
+}
